b9991834c9f69b9d9aaef3128114278a6ea7ad9e
Jonasjoja <43268787+Jonasjoja@users.noreply.github.com>
2020-02-14 08:41:21 +0100

    Add files via upload

---
+++ BusinessCard_w06/BusinessCard_w06/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  BusinessCard_w06
+//
+//  Created by admin on 07/02/2020.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func Page2(_ sender: Any) {
+    }
+    
+}
+
